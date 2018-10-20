7481de95ad9c35636441c2b4317c69982325b16c
ukangur <34269763+ukangur@users.noreply.github.com>
2018-10-20 19:23:07 +0300

    Initial commit

---
+++ instagram_scraper/tests/Converter.py
@@ -0,0 +1,22 @@
+import time
+import datetime
+import os
+import json
+#run setup first and when entering location (add the instagram location id)
+
+def testquery():
+    location = input('Asukoht: ')
+    date = input('Kuupäev (e.g dd/mm/yyyy) : ')
+
+    unix = time.mktime(datetime.datetime.strptime(date, "%d/%m/%Y").timetuple())
+
+    os.system('instagram-scraper ' + location + ' --location -m 60 --media-metadata -u urmaskkk -p OssaPoiss1')
+
+    input_dict = json.loads("C:/Users/Laptop/Desktop/funtimes/folder.json")
+
+    output_dict = [x for x in input_dict if x['timestamp'] in range(unix-43200,unix+43200)]
+
+    output_json = json.dumps("C:/Users/Laptop/Desktop/funtimes")
+
+testquery()
+
